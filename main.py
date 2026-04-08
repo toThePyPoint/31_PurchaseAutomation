3da0a108866067510522033a96c18e6d2c34a95f
toThePyPoint <tothepypoint@gmail.com>
2026-04-08 08:50:35 +0200

    Add exception handler.

--- main.py
+++ main.py
@@ -72,7 +72,8 @@ supplier_files_dict = {
     "DAFA": ["CONS_239", "CONS_239_2"],
     "RETECH": ["CONS_239", "CONS_239_2"],
     "KABEL_MAIER": ["CONS_239", "CONS_239_2"],
-    "GRUNEFELD": ["CONS_239", "CONS_239_2"]
+    "GRUNEFELD": ["CONS_239", "CONS_239_2"],
+    "HAFELE": ["CONS_234", "CONS_234_2"],
 }
 
 
@@ -90,81 +91,85 @@ export_files_directory_path = r"\\rfmesrv5\connect\DST_SAP_Transfer\P11\PPS_LUB\
 # Updating the open orders data
 
 movement_types = ('261', '313')
+try:
+    zek103_content = pd.read_excel(zek103_file_path, dtype=zek103_dtypes)
+    mb52df = get_mb52_data(mb52_file_path, mb52_dtypes)
 
-zek103_content = pd.read_excel(zek103_file_path, dtype=zek103_dtypes)
-mb52df = get_mb52_data(mb52_file_path, mb52_dtypes)
-
-# DONE: Get dfs for all exported files
-unique_mb51_files_dict = extract_unique_mb51_files(supplier_files_dict)
-MB551_dfs = load_grouped_mb51_dataframes(
-    grouped_files=unique_mb51_files_dict,
-    base_path=export_files_directory_path,
-    extension=".xlsx",
-    dtypes_mb51=mb51_consumption_dtypes,
-    col_names_mb51=MB51_new_col_names
-)
-
-
-# TODO: Change path
-supplier_files_paths = list_excel_files(supplier_files_directory_path_test)
-
-storage_locs = ('0003', '0004', '0005', '0007', '0710')
-
-current_year = datetime.date.today().year
-years_list = [current_year - 1, current_year, current_year + 1]
-pl_holidays = holidays.Poland(years=years_list)
-
-today = datetime.datetime.today().date()
-year_ago = today - datetime.timedelta(days=365)
-
-past_20_days_now = get_past_workdays(today, 20, 'last', pl_holidays)
-past_20_days_year_ago = get_past_workdays(year_ago, 20, 'last', pl_holidays)
-next_20_days_year_ago = get_past_workdays(year_ago, 20, 'next', pl_holidays)
-past_60_days_now = get_past_workdays(today, 60, 'last', pl_holidays)
-past_60_days_year_ago = get_past_workdays(year_ago, 60, 'last', pl_holidays)
-
-usage_parameters = [(next_20_days_year_ago, 'C'),
-                    (past_20_days_now, 'D'),
-                    (past_20_days_year_ago, 'E'),
-                    (past_60_days_now, 'F'),
-                    (past_60_days_year_ago, 'G'),
-]
-
-for file_path in supplier_files_paths:
-    plant = retrieve_plant(file_path)
-    excel_sheet = f"{plant}_data"
-
-    # DONE: Retrieve supplier name from file path and get corresponding df
-    supplier_name = retrieve_supplier_name(file_path)
-    z_mat_or_mb51_consumption_df = MB551_dfs[supplier_files_dict[supplier_name][0]]
-
-    sap_list = get_supplier_sap_numbers(file_path, excel_sheet)
-    zek103_output = get_zek103_data(zek103_content, plant, sap_list)
-    update_excel_with_quantities(file_path, zek103_output, 'SAP', 'CONSUMPTION', excel_sheet, True)
-
-    mb52 = filter_mb52_data(mb52df, sap_list, plant, storage_locs)
-    # Wywołanie funkcji
-    update_excel_with_dataframe(
-        file_path=file_path,
-        dataframe=mb52,
-        sap_column='A',  # Kolumna z numerami SAP w Excelu
-        frei_column='K',  # Kolumna, do której wpisywane są dane
-        header_start='Stock',  # Nagłówek wskazujący początek
-        header_end='S.C',       # Nagłówek wskazujący koniec
-        sheet_name=excel_sheet,
+    # DONE: Get dfs for all exported files
+    unique_mb51_files_dict = extract_unique_mb51_files(supplier_files_dict)
+    MB551_dfs = load_grouped_mb51_dataframes(
+        grouped_files=unique_mb51_files_dict,
+        base_path=export_files_directory_path,
+        extension=".xlsx",
+        dtypes_mb51=mb51_consumption_dtypes,
+        col_names_mb51=MB51_new_col_names
     )
 
-    for parameter in usage_parameters:
-        z_mat_consumption_grouped = filter_z_mat_consumption_data(z_mat_or_mb51_consumption_df, sap_list, parameter[0], plant)
 
-        # wpisanie danych do Excela
+    # TODO: Change path
+    supplier_files_paths = list_excel_files(supplier_files_directory_path_test)
+
+    storage_locs = ('0003', '0004', '0005', '0007', '0710')
+
+    current_year = datetime.date.today().year
+    years_list = [current_year - 1, current_year, current_year + 1]
+    pl_holidays = holidays.Poland(years=years_list)
+
+    today = datetime.datetime.today().date()
+    year_ago = today - datetime.timedelta(days=365)
+
+    past_20_days_now = get_past_workdays(today, 20, 'last', pl_holidays)
+    past_20_days_year_ago = get_past_workdays(year_ago, 20, 'last', pl_holidays)
+    next_20_days_year_ago = get_past_workdays(year_ago, 20, 'next', pl_holidays)
+    past_60_days_now = get_past_workdays(today, 60, 'last', pl_holidays)
+    past_60_days_year_ago = get_past_workdays(year_ago, 60, 'last', pl_holidays)
+
+    usage_parameters = [(next_20_days_year_ago, 'C'),
+                        (past_20_days_now, 'D'),
+                        (past_20_days_year_ago, 'E'),
+                        (past_60_days_now, 'F'),
+                        (past_60_days_year_ago, 'G'),
+    ]
+
+    for file_path in supplier_files_paths:
+        plant = retrieve_plant(file_path)
+        excel_sheet = f"{plant}_data"
+
+        # DONE: Retrieve supplier name from file path and get corresponding df
+        supplier_name = retrieve_supplier_name(file_path)
+        z_mat_or_mb51_consumption_df = MB551_dfs[supplier_files_dict[supplier_name][0]]
+
+        sap_list = get_supplier_sap_numbers(file_path, excel_sheet)
+        zek103_output = get_zek103_data(zek103_content, plant, sap_list)
+        update_excel_with_quantities(file_path, zek103_output, 'SAP', 'CONSUMPTION', excel_sheet, True)
+
+        mb52 = filter_mb52_data(mb52df, sap_list, plant, storage_locs)
+        # Wywołanie funkcji
         update_excel_with_dataframe(
             file_path=file_path,
-            dataframe=z_mat_consumption_grouped,
+            dataframe=mb52,
             sap_column='A',  # Kolumna z numerami SAP w Excelu
-            frei_column=parameter[1],  # Kolumna, do której wpisywane są dane
-            header_start='Consumption',  # Nagłówek wskazujący początek
-            header_end='Stock',       # Nagłówek wskazujący koniec
+            frei_column='K',  # Kolumna, do której wpisywane są dane
+            header_start='Stock',  # Nagłówek wskazujący początek
+            header_end='S.C',       # Nagłówek wskazujący koniec
             sheet_name=excel_sheet,
-            col_name='quantity'
         )
+
+        for parameter in usage_parameters:
+            z_mat_consumption_grouped = filter_z_mat_consumption_data(z_mat_or_mb51_consumption_df, sap_list, parameter[0], plant)
+
+            # wpisanie danych do Excela
+            update_excel_with_dataframe(
+                file_path=file_path,
+                dataframe=z_mat_consumption_grouped,
+                sap_column='A',  # Kolumna z numerami SAP w Excelu
+                frei_column=parameter[1],  # Kolumna, do której wpisywane są dane
+                header_start='Consumption',  # Nagłówek wskazujący początek
+                header_end='Stock',       # Nagłówek wskazujący koniec
+                sheet_name=excel_sheet,
+                col_name='quantity'
+            )
+
+except Exception as e:
+    print("Error: {}".format(e))
+    input("Press Enter to continue...")
